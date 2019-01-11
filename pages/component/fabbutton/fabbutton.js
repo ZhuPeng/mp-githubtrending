7a900123196c2e300495ae27cc5ac33262b81066
Peng Zhu <zhupengbupt@gmail.com>
2019-01-11 22:34:10 +0800

    extract fab button

---
+++ pages/component/fabbutton/fabbutton.js
@@ -0,0 +1,56 @@
+// pages/component/fabbutton/fabbutton.js
+const share = "../../../image/Paper-Plane.png"
+const chat = "../../../image/Chat.png"
+const top = "../../../image/up.png"
+const buttons = [{
+  label: "Top",
+  icon: top,
+}, {
+  openType: 'share',
+  label: 'Share',
+  icon: share,
+}, {
+  openType: 'contact',
+  label: 'Contact',
+  icon: chat,
+}]
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    types: ['topLeft', 'topRight', 'bottomLeft', 'bottomRight', 'center'],
+    typeIndex: 3,
+    colors: ['light', 'stable', 'positive', 'calm', 'balanced', 'energized', 'assertive', 'royal', 'dark'],
+    colorIndex: 0,
+    dirs: ['horizontal', 'vertical', 'circle'],
+    dirIndex: 0,
+    sAngle: 0,
+    eAngle: 360,
+    spaceBetween: 10,
+    buttons,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onFabClick(e) {
+      console.log('onFabClick', e.detail)
+      if (e.detail.index == 0) {
+        wx.pageScrollTo({
+          scrollTop: 0,
+          duration: 300
+        })
+      }
+    },
+  }
+})
--- pages/github/index.js
+++ pages/github/index.js
@@ -3,25 +3,6 @@ const app = getApp()
 const db = dbutil.getDB()
 const _ = db.command
 
-const share = "../../../image/Paper-Plane.png"
-const chat = "../../../image/Chat.png"
-const top = "../../../image/up.png"
-const buttons = [{
-      label: "Top",
-      icon: top,
-    },
-    {
-        openType: 'share',
-        label: 'Share',
-        icon: share,
-    },
-    {
-      openType: 'contact',
-      label: 'Contact',
-      icon: chat,
-    }
-]
-
 Page({
   data: {
     searchValue: "",
@@ -36,19 +17,6 @@ Page({
     orderList: ['时间', 'Star', 'Fork'],
     orderMap: {'时间': '_crawl_time', 'Star': 'star', 'Fork': 'fork'},
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-
-
-    types: ['topLeft', 'topRight', 'bottomLeft', 'bottomRight', 'center'],
-    typeIndex: 3,
-    colors: ['light', 'stable', 'positive', 'calm', 'balanced', 'energized', 'assertive', 'royal', 'dark'],
-    colorIndex: 0,
-    dirs: ['horizontal', 'vertical', 'circle'],
-    dirIndex: 0,
-    sAngle: 0,
-    eAngle: 360,
-    spaceBetween: 10,
-    buttons,
-
     spinning: true
   },
 
@@ -173,16 +141,6 @@ Page({
       })
       console.log("searchValue:", this.data.searchValue)
       this.search(e.detail, false)
-  },
-
-  onFabClick(e) {
-    console.log('onFabClick', e.detail)
-    if (e.detail.index == 0){
-        wx.pageScrollTo({
-            scrollTop: 0,
-            duration: 300
-        })
-    }
   },
 
   getOrder() {
