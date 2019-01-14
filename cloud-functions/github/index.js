7f0dc69ec9310500959886c1e140c30c192e71b9
ZhuPeng <zhupengbupt@gmail.com>
2019-01-14 23:56:39 +0800

    Add trace for api request

--- cloud-functions/github/index.js
+++ cloud-functions/github/index.js
@@ -9,13 +9,45 @@ db.collection("admin").where({website: "github", type: "token"}).get().then(res 
     token: res[0].value
   })
 })
-
 const per_page = 30;
 const page = 1;
+
+function dateFtt(fmt, date) { //author: meizz   
+  var o = {
+    "M+": date.getMonth() + 1,                 //月份   
+    "d+": date.getDate(),                    //日   
+    "h+": date.getHours(),                   //小时   
+    "H+": date.getHours(),                   //小时   
+    "m+": date.getMinutes(),                 //分   
+    "s+": date.getSeconds(),                 //秒   
+    "q+": Math.floor((date.getMonth() + 3) / 3), //季度   
+    "S": date.getMilliseconds()             //毫秒   
+  };
+  if (/(y+)/.test(fmt))
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+  for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt))
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+  return fmt;
+} 
+
+function trace(OPENID, owner, repo, type) {
+  db.collection('history').add({
+    data: {
+      openid: OPENID,
+      owner,
+      repo,
+      type,
+      requesttime: dateFtt("yyyy-MM-dd HH:mm:ss.S", new Date()),
+    }
+  }).then(res => { console.log(res) }).catch(console.error)
+}
 
 // 云函数入口函数
 exports.main = async (event, context) => {
   var {owner, repo, type} = event;
+  const { OPENID, APPID } = cloud.getWXContext()
+  trace(OPENID, owner, repo, type)
   var ref = 'master';
   var res;
   if (!type || type == "readme") {
