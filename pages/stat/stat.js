8d2fe9b06b3d74a6679e6040420796530c3875a5
Peng Zhu <zhupengbupt@gmail.com>
2019-01-27 02:37:09 +0800

    Add stat page with markdown

--- pages/stat/stat.js
+++ pages/stat/stat.js
@@ -7,31 +7,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-    total: 0,
     users: ["ooqTr4hMz05xwBHOU8VgokLyyoU0"],
-    count: [],
+    log: '',
+    total: '',
+    devtotal: ''
   },
 
   stat: function () {
+    var self = this;
     db.collection("history").count().then(res => {
-      this.setData({total: res.total})
+      self.setData({total: "## 访问量统计\n* Total: " + res.total + '\n'})
     });
-    var count = [];
+
     this.data.users.map(u => {
       var d = {openid: u}
+      var md = ''
       db.collection("history").where({openid: u}).count().then(res => {
-        d['total'] = res.total;
-        count.push(d)
-        this.setData({ count: count })
-      });    
+        md += '* 开发 ' + u.substring(6, 20) + "：" + res.total + '\n'
+        self.setData({ devtotal: md })
+      }); 
     })
 
     db.collection('history').where({
       openid: _.neq(this.data.users[0])
     }).orderBy('requesttime', 'desc').get().then(res => {
+      var log = ''
+      log += '## 访问记录\nrequesttime | behavior | repo | user \n-- | -- | -- | -- \n'
       res.data.map(d => {
-        console.log(d.type, d.openid, d.requesttime)
+        log += d.requesttime.substring(0, 19) + ' | ' + d.type + ' | ' + d.owner+'/'+d.repo + ' | ' + d.openid.substring(6, 12) + '\n';
       })
+      self.setData({log: log})
     })
   },
 
