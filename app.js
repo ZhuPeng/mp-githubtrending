b85afd6b148329256f3202ad30616258ac492613
Peng Zhu <zhupengbupt@gmail.com>
2019-03-01 13:35:10 +0800

    Update app onShow logic for redirect github page

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const util = require('./utils/util.js')
 App({
   onLaunch: function (options) {
+  },
+  onShow: function (options) {
     var path = options.path
-    console.log('onLaunch path: ', path)
+    console.log('onShow path: ', path)
     if (!util.isGitHubPage(path)) {
       return
     }
     var [owner, repo, filepath] = util.parseGitHub(path)
     console.log("parseGitHub url:", owner, repo, filepath)
-    if (owner == "") {return}
+    if (owner == "") { return }
     if (filepath == "" && repo == "") {
-      wx.navigateTo({ url: '/pages/account/account?owner=' + owner })
+      wx.redirectTo({ url: '/pages/account/account?owner=' + owner })
     } else if (filepath == "") {
-      wx.navigateTo({ url: '/pages/readme/readme?repo=' + owner + " / " + repo })
-    } else if (filepath.endsWith('.md') || util.isCodeFile(filepath)) {
-      wx.navigateTo({
+      wx.redirectTo({ url: '/pages/readme/readme?repo=' + owner + " / " + repo })
+    } else {
+      wx.redirectTo({
         url: '/pages/gitfile/gitfile?file=' + filepath + '&owner=' + owner + '&repo=' + repo,
       })
     }
