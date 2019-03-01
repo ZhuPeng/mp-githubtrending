b63246104bb04a7e8df46ef27f5d4bd72b9c88ec
Peng Zhu <zhupengbupt@gmail.com>
2019-03-01 23:14:20 +0800

    Add cache hit rates in stat page

--- pages/stat/stat.js
+++ pages/stat/stat.js
@@ -1,4 +1,5 @@
 const dbutil = require('../../utils/db.js')
+const util = require('../../utils/util.js')
 var db = dbutil.getDB()
 const _ = db.command
 Page({
@@ -6,14 +7,29 @@ Page({
     users: ["ooqTr4hMz05xwBHOU8VgokLyyoU0"],
     log: '',
     total: '',
-    devtotal: ''
+    devtotal: '',
+    cacheMiss: 0,
+    cachePercent: 0,
+    history: []
+  },
+
+  onLoad: function() {
+    this.stat()
   },
 
   stat: function () {
     var self = this;
+    db.collection("history").where({ fromcache: '0' }).count().then(res => {
+      var miss = res.total
+      db.collection("history").where({ fromcache: '1' }).count().then(res => {
+        var md = '* 缓存命中率 ' + util.GetPercent(res.total, res.total + miss)
+        self.setData({ cachePercent: md })
+      });
+    });
     db.collection("history").count().then(res => {
       self.setData({total: "## 访问量统计\n* Total: " + res.total + '\n'})
     });
+
 
     this.data.users.map(u => {
       var d = {openid: u}
@@ -23,16 +39,34 @@ Page({
         self.setData({ devtotal: md })
       }); 
     })
-
-    db.collection('history').where({
-      openid: _.neq(this.data.users[0])
-    }).orderBy('requesttime', 'desc').get().then(res => {
-      var log = ''
-      log += '## 访问记录\nrequesttime | behavior | repo | user \n-- | -- | -- | -- \n'
-      res.data.map(d => {
-        log += d.requesttime.substring(0, 19) + ' | ' + d.type + ' | ' + d.owner+'/'+d.repo + ' | ' + d.openid.substring(6, 12) + '\n';
-      })
-      self.setData({log: log})
-    })
+    self.loadHistory(0)
   },
+
+  genHistoryTable: function (history) {
+    var log = ''
+    log += '## 访问记录\ntime | type | repo | path | cache \n-- | -- | -- | -- | -- \n'
+    history.map(d => {
+      if (d.openid == this.data.users[0]) { return }
+      log += d.requesttime.substring(0, 19) + ' | ' + d.type + ' | ' + d.owner + '/' + d.repo + ' | ' + d.path +  '|' + d.fromcache + '\n';
+    })
+    this.setData({ log: log })
+  },
+
+  loadHistory: function (skip) {
+    var self = this
+    var history = this.data.history
+    db.collection('history').orderBy('requesttime', 'desc').skip(skip).get().then(res => {
+      res.data.map(d => {
+        if (d.openid != self.data.users[0]) {
+          history.push(d)
+        }
+      })
+      self.setData({history})
+      if (history.length > 50) {
+        self.genHistoryTable(history)
+        return
+      }
+      self.loadHistory(skip + 20)
+    })
+  }
 })
