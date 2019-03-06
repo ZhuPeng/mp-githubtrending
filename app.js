644b8bfa30015ef7e3b36f6a531aee066aff9146
ZhuPeng <zhupengbupt@gmail.com>
2019-03-06 21:28:31 +0800

    add listen for app global error

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const util = require('./utils/util.js')
 App({
   onLaunch: function (options) {
   },
+  onError(error) {
+    wx.showToast({
+      icon: 'none',
+      title: error,
+      duration: 2000
+    })
+  },
   onShow: function (options) {
     var path = options.path
     console.log('onShow path: ', path)
