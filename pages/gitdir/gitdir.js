4e0978a075c0dd0fbbe9d7a6c3352013242866b5
ZhuPeng <zhupengbupt@gmail.com>
2019-03-08 23:11:39 +0800

    Add dir browser page

---
+++ pages/gitdir/gitdir.js
@@ -0,0 +1,23 @@
+const cloudclient = require('../../utils/cloudclient.js')
+const util = require('../../utils/util.js')
+Page({
+  data: {
+    apiurl: '',
+    owner: '',
+    repo: '',
+    list: [],
+  },
+
+  onLoad: function (options) {
+    console.log(options)
+    this.setData({apiurl: options.apiurl, owner: options.owner, repo: options.repo})
+    var self = this
+    cloudclient.callFunction({ type: 'get', path: options.apiurl }, function (c) {
+      self.setData({ list: c })
+    })
+  },
+
+  onShareAppMessage: function () {
+
+  }
+})
