e192a0feb7f1604a7cae272cb8ca03f76153033e
Peng Zhu <zhupengbupt@gmail.com>
2019-03-24 23:53:00 +0800

    add blog card component

---
+++ pages/component/blogcard/blogcard.js
@@ -0,0 +1,28 @@
+Component({
+  properties: {
+    id: {
+      type: Int32Array,
+      value: ''
+    },
+    title: {
+      type: String,
+      value: ''
+    },
+    jobname: {
+      type: String,
+      value: ''
+    },
+    image_url: {
+      type: String,
+      value: ''
+    },
+  },
+
+  data: {
+
+  },
+
+  methods: {
+
+  }
+})
