0406be0fe94906f28f70930c8b0d27e08cf2d67b
Peng Zhu <zhupengbupt@gmail.com>
2019-04-21 00:20:45 +0800

    Add like +1 for collect formid

--- pages/component/subscribe/subscribe.js
+++ pages/component/subscribe/subscribe.js
@@ -18,7 +18,17 @@ Component({
   methods: {
     onButtonClick(e) {
       var self = this;
+      console.log(e)
       cloudclient.uploadFormID(e.detail.formId, 'Subscribe')
+      var text = e.detail.target.dataset.text || ''
+      if (text.indexOf('like') >= 0) {
+        wx.showToast({
+          title: 'Like +1',
+          duration: 1000,
+          success: function () {}
+        })
+        return
+      }
       cloudclient.callFunctionWithSubscribe({ action: this.data.status.nextAction }, function (r) {
         self.setData({ status: r })
       })
