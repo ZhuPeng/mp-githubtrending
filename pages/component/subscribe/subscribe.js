539876f0e5dfbf53a1c5d2db6ed3b866db3227a0
Peng Zhu <zhupengbupt@gmail.com>
2019-04-21 00:23:43 +0800

    Distinc formid source from like and subscribe

--- pages/component/subscribe/subscribe.js
+++ pages/component/subscribe/subscribe.js
@@ -19,9 +19,10 @@ Component({
     onButtonClick(e) {
       var self = this;
       console.log(e)
-      cloudclient.uploadFormID(e.detail.formId, 'Subscribe')
+      
       var text = e.detail.target.dataset.text || ''
       if (text.indexOf('like') >= 0) {
+        cloudclient.uploadFormID(e.detail.formId, 'Like')
         wx.showToast({
           title: 'Like +1',
           duration: 1000,
@@ -29,6 +30,7 @@ Component({
         })
         return
       }
+      cloudclient.uploadFormID(e.detail.formId, 'Subscribe')
       cloudclient.callFunctionWithSubscribe({ action: this.data.status.nextAction }, function (r) {
         self.setData({ status: r })
       })
