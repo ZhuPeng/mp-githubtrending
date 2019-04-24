6abd234b2823f7adb5084b6bd5f9bb522397604b
ZhuPeng <zhupengbupt@gmail.com>
2019-04-24 22:45:31 +0800

    add trending stat for pub

--- cloud-functions/triggerpub/index.js
+++ cloud-functions/triggerpub/index.js
@@ -14,6 +14,7 @@ exports.main = async (event, context) => {
   }
   var publist = []
   var content = await pubSimpleContent()
+  var trending = await trendingContent() 
   if (type == 'all' && OPENID == 'ooqTr4hMz05xwBHOU8VgokLyyoU0') {
     var res = await db.collection('subscribe').get()
     publist = res.data
@@ -32,7 +33,7 @@ exports.main = async (event, context) => {
       res.push(err)
       continue
     }
-    var r = await pub(publist[i].openid, f.formId, content, f._id)
+    var r = await pub(publist[i].openid, f.formId, content, trending, f._id)
     res.push(r)
   } 
   return {status: '操作完成', detail: res}
@@ -50,7 +51,39 @@ async function getFormId(openid) {
   return formid.data[0]
 }
 
-async function pub(openid, formId, content, id) {
+async function trendingContent() {
+  var now = new Date()
+  var start = now.getFullYear() + '-' + now.getMonth() + '-' + now.getDate()
+  var res = []
+  for (var i = 0; i<10; i++ ) {
+    var tmp = await db.collection('github').orderBy('_crawl_time', 'desc').skip(res.length).get()
+    res.push(...tmp.data)
+  }
+  
+  var count = {}
+  res.map(function(g) {
+    if (!(g.lang in count)) {count[g.lang] = 0} 
+    count[g.lang] += 1
+  })
+
+  var sorted = []
+  for (var k in count) {
+    sorted.push({ lang: k, cnt: count[k] });
+  }
+  function sortByCount(a, b) {
+    return b.cnt - a.cnt;
+  };
+
+  sorted.sort(sortByCount);
+  var content = 'Trending 最近更新项目语言 TOP5'
+  sorted.map(function(s, i) {
+    if (i >= 5) {return}
+    content += '\n* ' + s.lang + ': ' + s.cnt
+  })
+  return content
+}
+
+async function pub(openid, formId, content, trending, id) {
   console.log('pub:', openid, formId)
   try {
     const result = await cloud.openapi.templateMessage.send({
@@ -58,7 +91,7 @@ async function pub(openid, formId, content, id) {
       page: 'pages/github/index',
       data: {
         keyword1: {
-          value: 'GitHub Trending 更新 666 个项目' 
+          value: trending,
         },
         keyword2: {
           value: content
@@ -72,7 +105,7 @@ async function pub(openid, formId, content, id) {
     return result
   } catch (err) {
     console.log('pub error:', err)
-    if (err.errMsg.indexOf('form id used count reach limit') >= 0) {
+    if (err.errMsg && err.errMsg.indexOf('form id used count reach limit') >= 0) {
       await deleteFormId(id)
     }
     return err
