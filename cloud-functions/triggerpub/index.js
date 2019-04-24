e8a535f82d23f3bbeda52dabaffeb65871651754
ZhuPeng <zhupengbupt@gmail.com>
2019-04-24 22:04:06 +0800

    only select a week ago formid

--- cloud-functions/triggerpub/index.js
+++ cloud-functions/triggerpub/index.js
@@ -2,6 +2,7 @@ const cloud = require('wx-server-sdk')
 var rp = require('request-promise');
 cloud.init()
 const db = cloud.database()
+const _ = db.command
 
 exports.main = async (event, context) => {
   const {OPENID} = cloud.getWXContext()
@@ -38,8 +39,9 @@ exports.main = async (event, context) => {
 }
 
 async function getFormId(openid) {
-  // formid 有效期7天内
-  var formid = await db.collection('formid').where({openid, status: 'new'}).limit(1).get()
+  var validDate = new Date()
+  validDate.setDate(validDate.getDate() - 7);
+  var formid = await db.collection('formid').where({ openid, status: 'new', time: _.gt(validDate) }).orderBy('time', 'asc').limit(1).get()
   // console.log(formid)
   if (formid.data.length == 0) {
     console.log('没有找到可用的 formid:', openid)
