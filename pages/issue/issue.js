777044bda0d360f93f7b9be09ce6824e98276e95
Peng Zhu <zhupengbupt@gmail.com>
2019-05-01 11:01:40 +0800

    Add close and reopen issue button

--- pages/issue/issue.js
+++ pages/issue/issue.js
@@ -2,6 +2,7 @@ const cloudclient = require('../../utils/cloudclient.js')
 const util = require('../../utils/util.js')
 Page({
   data: {
+    localowner: wx.getStorageSync("github-name") || '',
     issue: {},
     comments: [],
     prDiff: '',
@@ -15,6 +16,19 @@ Page({
     this.setData({ content: e.detail.value })
   },
 
+  onButtonCloseClick: function(e) {
+    var { owner, repo } = this.data
+    var self = this
+    var state = this.data.issue.state
+    cloudclient.callFunction({ type: 'post', path: '/repos/' + owner + '/' + repo + '/issues/' + this.data.issue.number, 'state': state == 'open'?'closed':'open' }, function (c) {
+      console.log(c)
+      var tmp = self.data.issue
+      tmp.state = c.state
+      self.setData({issue: tmp})
+      util.Alert('Success', 4000)
+    })
+  },
+
   onButtonClick: function () {
     if (this.data.content == '') {
       util.Alert('Content was empty!', 4000)
@@ -22,7 +36,7 @@ Page({
     var {owner, repo} = this.data
     cloudclient.callFunction({ type: 'post', path: '/repos/' + owner + '/' + repo + '/issues/' + this.data.issue.number + '/comments', body: this.data.content, owner, repo }, function (c) {
       console.log(c)
-      util.Alert('Create Success', 4000)
+      util.Alert('Success Create', 4000)
       wx.navigateTo({
         url: '/pages/issue/issue?issue=' + c.issue_url,
       })
