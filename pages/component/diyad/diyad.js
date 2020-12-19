941830d4383ad209989955a83e19b37d16f35bea
Peng Zhu <zhupengbupt@gmail.com>
2020-12-19 14:42:03 +0800

    Update books recommnad when click next

--- pages/component/diyad/diyad.js
+++ pages/component/diyad/diyad.js
@@ -5,6 +5,13 @@ Component({
       type: String,
       value: ''
     },
+    num: {
+      type: String,
+      value: '',
+      observer() {
+        this.reload_blog();
+      }
+    },
   },
 
   data: {
@@ -13,13 +20,17 @@ Component({
   },
 
   methods: {
-    binderror(e) {
-      console.log("ad render error:", e)
+    reload_blog() {
+      if (this.data.num == '') {return}
       var self = this
       cloudclient.callFunctionWithBlog({type: 'random'}, function (c){
         console.log('random blog:', c)
         self.setData({item: c['data'] || {}, adrendererror: 'true'})
       })
+    },  
+    binderror(e) {
+      console.log("ad render error:", e)
+      this.reload_blog()
     }
   }
 })
