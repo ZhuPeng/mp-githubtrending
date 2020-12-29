b63abc1f172d4ef8c3a6cacfc87cd3c321b3b760
Peng Zhu <zhupengbupt@gmail.com>
2020-12-29 21:54:26 +0800

    Add auto visit dir when java resource dir

--- pages/gitdir/gitdir.js
+++ pages/gitdir/gitdir.js
@@ -11,11 +11,19 @@ Page({
 
   onLoad: function (options) {
     console.log(options)
+    if (options.auto) {
+      util.Alert("已为你自动跳转到下一目录，请稍等", 2000)
+    }
     var apiurl = decodeURIComponent(options.apiurl)
     this.setData({apiurl: apiurl, owner: options.owner, repo: options.repo})
     var self = this
     cloudclient.callFunction({ type: 'get', path: options.apiurl, repo: options.repo, owner: options.owner }, function (c) {
       util.SetDataWithoutSpin(self, { list: c })
+      if (c.length == 1 && c[0].type == 'dir') {
+        wx.navigateTo({
+          url: '/pages/gitdir/gitdir?auto=true&owner=' + self.data.owner + '&repo=' + self.data.repo + '&apiurl=' + c[0].url,
+        })
+      }
     })
   },
 
