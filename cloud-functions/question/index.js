2609378c3958dfe877df2a737346910951204a98
Peng ZHU <zhupengbupt@gmail.com>
2021-10-07 23:15:36 +0800

    Support tag and cnt show

--- cloud-functions/question/index.js
+++ cloud-functions/question/index.js
@@ -12,31 +12,32 @@ exports.main = async (event, context) => {
 
 async function getRandom() {
     var total = {
-        'algo': 8,
-        'cplusplus': 1,
-        'db': 6,
-        'go': 23,
-        'java': 1,
-        'os': 14,
-        'js': 419,
+        'algo': {'cnt': 8, 'name': '算法'},
+        'cplusplus': {'cnt': 1, 'name': 'C++'},
+        'db': {'cnt': 6, 'name': '数据库'},
+        'go': {'cnt': 23, 'name': 'Go'},
+        'java': {'cnt': 1, 'name': 'Java'},
+        'os': {'cnt': 14, 'name': '操作系统'},
+        'js': {'cnt': 419, 'name': 'JavaScript'},
     }
     var total_cnt = 0
     for (k in total) {
-        total_cnt += total[k]
+        total_cnt += total[k]['cnt']
     }
     console.log('total question cnt:', total_cnt)
 
     var base_url = 'https://github.com/ZhuPeng/iDayDayUP/blob/main/'
     var r = Math.floor(Math.random() * (total_cnt || 0)) + 1
     for (k in total) {
-        if (r > total[k]) {
-            r -= total[k]
+        if (r > total[k]['cnt']) {
+            r -= total[k]['cnt']
             continue
         } 
         if (r < 10) { r = '0' + r}
         return {
           'answer_url': base_url + k + '/a' + r + '.md',
           'question_url': base_url + k + '/q' + r + '.md',
+          'meta': total[k],
         }
     }
 }
